3c9e085a05ecf5afc2d89baa119cf1bca372cfc9
Son Trinh <son1932001@gmail.com>
2023-11-02 20:06:16 +0700

    Authen User

---
+++ FushionFashion/BusinessObject/Dtos/Account/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessObject.Dtos.Account
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
--- FushionFashion/ServiceAuthentication/Services/IAuthenticationService.cs
+++ FushionFashion/ServiceAuthentication/Services/IAuthenticationService.cs
@@ -1,10 +1,13 @@
 ﻿using BusinessObject.Dtos.Account;
+using BusinessObject.Entities.Account;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace ServiceAuthentication.Services
 {
     public interface IAuthenticationService
     {
-        public Task<RegisterDtos> RegisterUser(RegisterDtos registerDtos);
+        public Task<AppUser> RegisterUser(RegisterDtos registerDtos);
+        public Task<AppUser> LoginUser(LoginDtos loginDtos);
     }
 }

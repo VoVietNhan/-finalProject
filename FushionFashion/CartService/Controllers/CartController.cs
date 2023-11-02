2c21b92b34f2d9bef31dced9a93ec5239e2bc89b
VoVietNhan <vovietnhan180802@gmail.com>
2023-11-02 18:54:23 +0700

    new cart controller

---
+++ FushionFashion/CartService/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessObject.Entities.Cart;
+using CartService.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace CartService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly CartContext _cartContext;
+        public CartController(CartContext cartContext)
+        {
+            _cartContext = cartContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Cart>> GetCartByUserId(Guid UserId)
+        {
+            var cart =  await _cartContext.Carts.FirstOrDefaultAsync(x => x.CreatedBy == UserId);
+            return cart;
+        }
+        [HttpPost("CreateCart")]
+        public async Task<ActionResult<Cart>> CreateCartByUserId(Guid UserId)
+        {
+            var checkCart = await GetCartByUserId(UserId);
+            if(checkCart.Result == null)
+            {
+                var cart = new Cart();
+                cart.CreatedBy = UserId;
+                cart.ApprovedBy = UserId;
+                cart.ModifiedBy = UserId;
+                cart.ApprovedDate = DateTime.Now;
+                cart.ModifiedDate = DateTime.Now;
+                cart.ApprovedDate = DateTime.Now;
+                cart.Status = BusinessObject.Enum.EnumStatus.EnumStatus.Enable;
+                await _cartContext.Carts.AddAsync(cart);
+                await _cartContext.SaveChangesAsync();
+                return cart;
+            }
+            return checkCart;
+        }
+    }
+}
---
+++ FushionFashion/CartService/Data/CartContext.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObject.Entities.Cart;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection.Emit;
+
+namespace CartService.Data
+{
+    public class CartContext : DbContext
+        
+    {
+        public CartContext() { }
+        public CartContext(DbContextOptions<CartContext> options) : base(options) { }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Cart>(entity =>
+            {
+                entity.HasMany(c => c.CartDetails)
+                      .WithOne()
+                      .HasForeignKey(cd => cd.CartId);
+            });
+
+            builder.Entity<CartDetail>(entity =>
+            {
+                entity.HasOne(cd => cd.Cart)
+                      .WithMany(c => c.CartDetails)
+                      .HasForeignKey(cd => cd.CartId);
+            });
+        }
+    }
+}

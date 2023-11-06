a769d5429a6deff2b4aee439067bd9315b63593a
MinhThuw <95035935+MinhThuuu@users.noreply.github.com>
2023-11-06 18:05:35 +0700

    client product

---
+++ FushionFashion/Admin/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObject.Dtos.Product;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri productUri = new Uri("https://localhost:44321/api/Product");
+        private readonly HttpClient _client;
+        public ProductController(HttpClient client)
+        {
+            _client = client;
+        }
+        public IActionResult Index()
+        {
+            List<ProductViewModel> productList = new List<ProductViewModel>();
+
+            HttpResponseMessage respone = _client.GetAsync(productUri + "/GetAllProduct").Result;
+            if (respone.IsSuccessStatusCode)
+            {
+                string data = respone.Content.ReadAsStringAsync().Result;
+                productList = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+
+            }
+            ViewBag.product = productList;
+            return View();
+        }
+    }
+}

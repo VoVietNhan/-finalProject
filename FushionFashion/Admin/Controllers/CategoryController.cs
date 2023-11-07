528afa287cb161fdbcc485c62e88802d3df9e54d
Son Trinh <son1932001@gmail.com>
2023-11-07 07:34:52 +0700

    add category

--- FushionFashion/Admin/Controllers/CategoryController.cs
+++ FushionFashion/Admin/Controllers/CategoryController.cs
@@ -1,35 +1,79 @@
 ﻿using BusinessObject.Dtos.Size;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System;
 using BusinessObject.Dtos.Category;
+using System.Net.Http.Headers;
+using BusinessObject.Dtos.Account;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BusinessObject.Entities.Product;
+using System.Text;
 
 namespace Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        Uri cateUri = new Uri("https://localhost:44321/api/Category");
-        private readonly HttpClient _client;
-        public CategoryController(HttpClient client)
-        {
-            _client = client;
-        }
-        public IActionResult Index()
-        {
-            List<CategoryViewModel> cateList = new List<CategoryViewModel>();
+        private readonly HttpClient client;
+        private readonly string CategoryApiUrl;
 
-            HttpResponseMessage respone = _client.GetAsync(cateUri + "/GetAllCategory").Result;
-            if (respone.IsSuccessStatusCode)
+        public CategoryController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            CategoryApiUrl = "https://localhost:44321/api/Category";
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(CategoryApiUrl + "/GetAllcategory");
+            string strData = await response.Content.ReadAsStringAsync();
+
+            var option = new JsonSerializerOptions
             {
-                string cate = respone.Content.ReadAsStringAsync().Result;
-                cateList = JsonConvert.DeserializeObject<List<CategoryViewModel>>(cate);
-            }
-            ViewBag.product = cateList;
+                PropertyNameCaseInsensitive = true,
+            };
+            var cate = JsonSerializer.Deserialize<List<CategoryViewModel>>(strData, option);
+            return View(cate);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"{CategoryApiUrl + "/GetProductById"}/{id}");
+            string strData = await response.Content.ReadAsStringAsync();
+
+            var option = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var cate = JsonSerializer.Deserialize<CategoryViewModel>(strData, option);
+            return View(cate);
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCategoryViewModel createCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonSerializer.Serialize(createCategoryViewModel);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(CategoryApiUrl + "/CreateCategory", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(createCategoryViewModel);
+        }
 
     }
 }
